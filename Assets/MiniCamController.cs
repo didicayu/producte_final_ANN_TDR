77aad37e298e2578f8bd708dc05fd1e9ad6bb8d0
Dídac Cayuela <didicayu@gmail.com>
2020-12-06 19:00:11 +0100

    Tracks now automatically playable

---
+++ Assets/MiniCamController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniCamController : MonoBehaviour
+{
+    public GameObject ParentObject;
+    Vector3 sum;
+    float FurtherAway;
+    public float offset = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FurtherAway = 0;
+        sum = Vector3.zero;
+        StartCoroutine(WaitFrames());
+    }
+    IEnumerator WaitFrames()
+    {
+        yield return 1;
+
+        DoPositionCalculationStuff();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DoPositionCalculationStuff()
+    {
+        for (int i = 0; i < ParentObject.transform.childCount; i++)
+        {
+            sum += ParentObject.transform.GetChild(i).position;
+            float result = Mathf.Max(Mathf.Abs(ParentObject.transform.GetChild(i).position.z - transform.position.z), FurtherAway);
+            FurtherAway = result;
+        }
+        sum = (sum / ParentObject.transform.childCount);
+
+        transform.position = new Vector3(sum.x, transform.position.y, sum.z);
+        gameObject.GetComponent<Camera>().orthographicSize = FurtherAway - offset;
+    }
+}
---
+++ Assets/skybox/TrackSpawner1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackSpawner1 : MonoBehaviour
+{
+    [Header("Nom del circuit")]
+    public string TrackName = "track";
+
+    public GameObject ParentObject;
+    [Header("Ficar els prefabs de les carreteres i l'agent")]
+    public GameObject[] Objectes;
+    public GameObject AgentCar;
+    public float HeightOffset = 1.75f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AgentCar.SetActive(true);
+        StartCoroutine(WaitXFrames());
+    }
+    IEnumerator WaitXFrames()
+    {
+        yield return 0;
+
+        AgentCar.SetActive(true);
+    }
+
+    private void Awake()
+    {
+        ParentObject = gameObject;
+        SpawnTrack();
+        Transform three = gameObject.transform.Find("3");
+        
+        AgentCar.transform.position = new Vector3(three.position.x, three.position.y + HeightOffset, three.position.z);
+        AgentCar.transform.rotation = three.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnTrack()
+    {
+        ParentObject.transform.localScale = new Vector3(1,1,1);
+
+        
+        for (int j = 0; j < ParentObject.transform.childCount; j++)
+        {
+            Transform trans = ParentObject.transform.GetChild(j);
+            Destroy(trans.gameObject);
+            
+        }
+
+        TrackData data = SaveSystem.LoadTrackWithName(TrackName);
+
+        for (int i = 0; i < data.TypesOfRoad.Length; i++)
+        {
+            GameObject go = Instantiate(Objectes[(int.Parse(data.TypesOfRoad[i]))], ParentObject.transform);
+            go.transform.position = new Vector3(data.positionsX[i], data.positionsY[i], data.positionsZ[i]);
+            go.transform.eulerAngles = new Vector3(0, data.rotation[i], 0);
+            go.name = data.TypesOfRoad[i];
+        }
+        ParentObject.transform.localScale *= 5;
+    }
+}
